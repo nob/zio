517cbd23f3ad1f26955bca167984b2fbf2e3f30b
nob <noboruthedragon@gmail.com>
2011-12-23 13:28:23 +0000

    border-radius for IE. Customized error page. SEO
    git-svn-id: https://koholasys.springloops.com/source/zionusbsm/branches/sharedserver@38 b66679b3-fe4a-4674-a71e-25c3c480966b

--- htdoc/system/application/errors/error_404.php
+++ htdoc/system/application/errors/error_404.php
@@ -2,33 +2,56 @@
 <head>
 <title>404 Page Not Found</title>
 <style type="text/css">
-
-body {
-background-color:	#fff;
-margin:				40px;
-font-family:		Lucida Grande, Verdana, Sans-serif;
-font-size:			12px;
-color:				#000;
-}
-
-#content  {
-border:				#999 1px solid;
-background-color:	#fff;
-padding:			20px 20px 12px 20px;
-}
-
-h1 {
-font-weight:		normal;
-font-size:			14px;
-color:				#990000;
-margin: 			0 0 4px 0;
-}
+    body {
+        background-color:	#FFF;
+        margin:				0px;
+        font-family:		Lucida Grande, Verdana, Sans-serif;
+        font-size:			16px;
+        color:				#9B9B9B;
+    }
+    div#logo {
+        margin: 50px auto 0px;
+        width: 180px;
+    }
+    h1 {
+        color: #9B9B9B;
+        font-size: 38px;
+        margin: 25px auto 10px;
+        width: 600px;
+        text-align: center;
+    }
+    p {
+        text-align: center;
+        margin-bottom: 80px
+    }
+    a#back {
+        display:block;
+        background-color: #dddddd;
+        color: #457039;
+        border-radius: 10px;
+        margin: 0 auto;
+        width: 120px;
+        padding: 10px 5px;
+        font-size: 16px;
+        text-align: center;
+    }
+    a#back:hover {
+        color:#71B75D;
+    }
 </style>
+<script type="text/javascript">
+    setTimeout('redirect()', 3000);
+    function redirect() {
+        location.href = 'http://www.zionusbsm.com/';
+    }
+</script>
 </head>
 <body>
-	<div id="content">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+    <div id="logo">
+        <a href="/" alt="Zionus BSM"><img src="/img/logo.png" alt="Zionus BSM" width="180" height="115"/></a>
+    </div>
+    <h1><?php echo $heading; ?></h1>
+    <?php echo $message; ?>
+    <a id="back" href="/" title="back to home">back to home</a>
 </body>
 </html>
